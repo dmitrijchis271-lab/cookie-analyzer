1c7a338ed4ee783363f29aedde073c07d792bb44
dmitrijchis271-lab <dmitrijchis271@gmail.com>
2026-06-01 23:08:58 +0300

    Add Avalonia XAML UI with Russian language interface

---
+++ src/CookieAnalyzer.UI/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+namespace CookieAnalyzer.UI;
+
+using Avalonia.Controls;
+using CookieAnalyzer.UI.Views;
+using CookieAnalyzer.UI.ViewModels;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/CookieAnalyzer.UI/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using CookieAnalyzer.UI.ViewModels;
+
+namespace CookieAnalyzer.UI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
